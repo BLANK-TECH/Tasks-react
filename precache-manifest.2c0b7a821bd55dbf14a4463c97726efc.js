355e8f8ff08895cc1d57120f17e32a295876fdec
BLANK-TECH <ivonne_1412@hotmail.com>
2019-11-03 21:48:32 -0600

    Updates

---
+++ precache-manifest.2c0b7a821bd55dbf14a4463c97726efc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2058cde0f0a03d34d684d28ffd4c217b",
+    "url": "/Tasks-react/./index.html"
+  },
+  {
+    "revision": "d345df7e9166d142561c",
+    "url": "/Tasks-react/./static/js/2.9f19a723.chunk.js"
+  },
+  {
+    "revision": "766f99c15e527b4f37eb",
+    "url": "/Tasks-react/./static/js/main.e3013e71.chunk.js"
+  },
+  {
+    "revision": "8a8029639bbec57e73b9",
+    "url": "/Tasks-react/./static/js/runtime-main.304fb5ed.js"
+  }
+]);
